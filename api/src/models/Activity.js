c68029c5cc18999754faa57262bf5dd96a2c6290
FedeMas1 <javierjazmin@gmail.com>
2023-03-30 03:51:19 +0200

    first commit

---
+++ api/src/models/Activity.js
@@ -0,0 +1,29 @@
+const { DataTypes } = require('sequelize');
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+  // defino el modelo
+  sequelize.define('activity', {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    dificultad: {
+      type: DataTypes.ENUM('1','2','3','4','5'),
+      allowNull: false,
+    },
+    duracion: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    temporada: {
+      type: DataTypes.ENUM('Verano','Otoño','Invierno','Primavera'),
+      allowNull: false,
+    },
+  });
+};
